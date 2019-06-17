e94f4bacc3014cfd13216a56edad954a3e0cf3c4
morrislenny <scot0530@morris.umn.edu>
2019-06-17 12:05:49 -0400

    Add add-to-cart function and cart atom

--- src/workframe_bakery/client.cljs
+++ src/workframe_bakery/client.cljs
@@ -5,11 +5,29 @@
 
 (def treats (r/atom []))
 
+(def cart (r/atom []))
+
 (defn get-treats! []
   (http/GET "/treats"
             {:handler (fn [resp] (reset! treats (:treats resp)))
              :error-handler (fn [e] (.warn js/console "treats error" e))
              :response-format :json, :keywords? true}))
+
+(defn get-item-by-id
+  "returns first map with matching id when datastucture is:
+  [{:id ~ ...} {:id~ ....} ...]
+  ASSUMPTION FOR CORRECT BEHAVOIR: id's in maps are unique"
+  [vec-of-maps id]
+  (-> (filter #(= id (:id %)) vec-of-maps)
+      first))
+
+(defn add-to-cart
+  [id]
+  (let [index (->> (get-item-by-id @cart id)
+                   (.indexOf @cart))]
+    (if (> index -1)
+      (swap! cart update-in [index :quantity] inc)
+      (swap! cart conj {:id id :quantity 1}))))
 
 (defn main-app-component
   []
